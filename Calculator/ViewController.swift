eaa1135f2e9fc8a405b486ec214311e624974f90
wdsand <wdsand@thoughtworks.com>
2017-03-29 16:28:31 -0400

    added autolayout and connected to controller

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
 
+    @IBAction func mathematicalOperation(_ sender: Any) {
+    }
+    @IBAction func digit(_ sender: Any) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
