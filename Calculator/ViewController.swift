06ca112d0cbed26224708799bd66dd37afe56037
wdsand <wdsand@thoughtworks.com>
2017-03-30 10:22:21 -0400

    added all buttons for assign 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
+    var currentUserIsTyping = false
 
     @IBAction func mathematicalOperation(_ sender: Any) {
     }
-    @IBAction func digit(_ sender: Any) {
+    
+    @IBAction func digit(_ sender: UIButton) {
+    let digit = sender.currentTitle!
+        if currentUserIsTyping{
+    let currentDisplayText = display.text!
+    display.text! = currentDisplayText + digit
+        }else{
+        display.text! = digit
+        }
+        currentUserIsTyping = true
     }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
     override func didReceiveMemoryWarning() {
