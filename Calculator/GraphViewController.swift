bfcddadde5409965b8b7103b4f73f8e3fb999170
wdsand <wdsand@thoughtworks.com>
2017-04-04 11:50:04 -0400

    pan gestured added

--- Calculator/GraphViewController.swift
+++ Calculator/GraphViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class GraphViewController: UIViewController {
 
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                                  y:view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
+    
     
     @IBOutlet weak var graphView: GraphView!{
         didSet{
